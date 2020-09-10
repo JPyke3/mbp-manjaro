24f678d4533520f34a1cdeffc43eb87ad06f4c4f
JPyke3 <pyke.jacob1@gmail.com>
2020-09-10 10:01:14 +1000

    Fixed bug in bash script

--- Scripts/repo-all-editions.sh
+++ Scripts/repo-all-editions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for f in ../manjaro; do
+for f in ../manjaro/*; do
     if [ -d "$f" ]; then
+
         cp user-repos.conf ../manjaro/"$f"
     fi
 done
 
-for f in ../community; do
+for f in ../community/*; do
     if [ -d "$f" ]; then
         cp user-repos.conf ../manjaro/"$f"
     fi
