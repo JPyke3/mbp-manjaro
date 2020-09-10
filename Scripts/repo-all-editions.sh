8c678bf4861c6bacaae30da40d5ea11ab5ed885f
JPyke3 <pyke.jacob1@gmail.com>
2020-09-10 10:15:16 +1000

    Added symbolic user-profile.conf links, All mbp packages now stored in Packages-Root unless it isn't symlinked

--- Scripts/repo-all-editions.sh
+++ Scripts/repo-all-editions.sh
@@ -2,13 +2,12 @@
 
 for f in ../manjaro/*; do
     if [ -d "$f" ]; then
-
-        cp user-repos.conf ../manjaro/"$f"
+        ln -sf ../../user-repos.conf ../manjaro/"$f"/user-repos.conf 
     fi
 done
 
 for f in ../community/*; do
     if [ -d "$f" ]; then
-        cp user-repos.conf ../manjaro/"$f"
+        ln -sf ../../user-repos.conf ../community/"$f"/user-repos.conf 
     fi
 done
